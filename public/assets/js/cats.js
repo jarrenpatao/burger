b31005b5c9f5ff5507700b9236f925c722a367e1
jarrenpatao <jarrenpatao@gmail.com>
2018-12-21 10:30:10 -0800

    changing cats to burgers lol

--- public/assets/js/cats.js
+++ public/assets/js/cats.js
@@ -1,20 +1,20 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
-    var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
+  $(".change-hungry").on("click", function(event) {
+    let id = $(this).data("id");
+    let newHungry = $(this).data("newhungry");
 
-    var newSleepState = {
-      sleepy: newSleep
+    let newHungryState = {
+      hungry: newHungry
     };
 
     // Send the PUT request.
     $.ajax("/api/cats/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: newHungryState
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
+        console.log("changed hungry to", newHungry);
         // Reload the page to get the updated list
         location.reload();
       }
@@ -25,33 +25,33 @@ $(function() {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
+    var newBurger = {
       name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+      hungry: $("[name=hungry]:checked").val().trim()
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
       type: "POST",
-      data: newCat
+      data: newBurger
     }).then(
       function() {
-        console.log("created new cat");
+        console.log("created new burger");
         // Reload the page to get the updated list
         location.reload();
       }
     );
   });
 
-  $(".delete-cat").on("click", function(event) {
+  $(".delete-burger").on("click", function(event) {
     var id = $(this).data("id");
 
     // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burger/" + id, {
       type: "DELETE"
     }).then(
       function() {
-        console.log("deleted cat", id);
+        console.log("deleted burger", id);
         // Reload the page to get the updated list
         location.reload();
       }
